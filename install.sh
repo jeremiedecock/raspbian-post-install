6339dd5e7bfcc2451b58e48e74145392aade92ed
Jérémie Decock <jd.jdhp@gmail.com>
2016-11-13 19:19:17 +0100

    Comment unchecked functions.

--- install.sh
+++ install.sh
@@ -88,7 +88,8 @@ pi_init_fn
 
 pi_confirm ${PI_SCRIPTS_COMMON_DIR}/setup_l10n.sh "Localization and internationalization (setup keyboard, locale and timezone)"
 
-pi_confirm ${PI_SCRIPTS_COMMON_DIR}/resize_partition.sh "Resize partitions"
+# TODO: update the following function
+#pi_confirm ${PI_SCRIPTS_COMMON_DIR}/resize_partition.sh "Resize partitions"
 
 # NTP works out of the box with recent raspbian releases
 #pi_confirm ${PI_SCRIPTS_COMMON_DIR}/install_ntp.sh "Install and configure ntp"
@@ -97,7 +98,8 @@ pi_confirm pi_pi_pwd "Change the 'pi' password to something more secure"
 
 pi_confirm pi_root_pwd "Change the root password to something more secure"
 
-pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/install_mpeg2_vc1_keys.sh "Install MPEG2 and VC1 license keys"
+# TODO: update the following function
+#pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/install_mpeg2_vc1_keys.sh "Install MPEG2 and VC1 license keys"
 
 pi_confirm ${PI_SCRIPTS_COMMON_DIR}/set_hostname.sh "Configure hostname"
 
@@ -109,12 +111,15 @@ pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/install_packages.sh "Install packages"
 
 pi_confirm ${PI_SCRIPTS_COMMON_DIR}/enable_ssh.sh "Enable SSH"
 
-pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/enable_spi.sh "Enable SPI"
+# TODO: update the following function
+#pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/enable_spi.sh "Enable SPI"
 
-pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/enable_picamera.sh "Enable RPI camera"
+# TODO: update the following function
+#pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/enable_picamera.sh "Enable RPI camera"
 
 pi_confirm ${PI_SCRIPTS_COMMON_DIR}/ssh_reconfigure.sh "Change the ssh host keys (as all rpi images have the same keys)"
 
-pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/memory_split.sh "Memory split"
+# TODO: update the following function
+#pi_confirm ${PI_SCRIPTS_RASPBIAN_DIR}/memory_split.sh "Memory split"
 
 pi_quit_fn
